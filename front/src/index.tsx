918edbb0c0cf00c1071b68813b1275aa97cbfbc5
ohteru <arai_da@camobile.com>
2019-03-16 15:48:37 +0900

    :spackles: react + webpack

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,15 +1,9 @@
-import * as React from 'react'; // 変更
-import * as ReactDOM from 'react-dom'; // 変更
-import App from './App';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { Hello } from "./components/Hello";
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <Hello compiler="TypeScript" framework="React" />,
+    document.getElementById("example")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
