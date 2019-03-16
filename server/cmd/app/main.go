46ea11882fb89f54bb69a2d2b5279b69e6df42df
kokikono <kk.k.0421@outlook.jp>
2019-03-16 16:02:21 +0900

    Wipデース：httpを起動したい

--- server/cmd/app/main.go
+++ server/cmd/app/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-  fmt.Printf("仕事したくない")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`
+			<!DOCTYPE html>
+			<html lang="ja">
+			<head>
+				<meta charset="UTF-8">
+				<title>ちょっとだけよ</title>
+			</head>
+			<body>
+				<h1>仕事したくない！！</h1>
+			</body>
+			</html>
+		`))
+	})
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
 }
