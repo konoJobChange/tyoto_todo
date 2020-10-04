460048b5186be5642aaa382ef4453d9950a5fddf
河野浩生 <61747641koki@gmail.com>
2020-10-04 15:00:50 +0900

    create api todo

---
+++ front/src/pages/api/todos.ts
@@ -0,0 +1,21 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export interface ToDo {
+  id: string;
+  title: string;
+  detail: string;
+  update_at: string;
+  create_at: string;
+}
+const data: ToDo[] = [1, 2, 3, 4, 5].map((v) => ({
+  id: `key_${v}`,
+  title: `toto_${v}`,
+  detail: `ちょっと頑張る${v}`,
+  update_at: new Date().toISOString(),
+  create_at: new Date().toISOString(),
+}));
+
+export default (req: any, res: any) => {
+  res.statusCode = 200;
+  res.json(data);
+};
