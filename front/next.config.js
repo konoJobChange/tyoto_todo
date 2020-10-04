6740de902e03e0cb146e2a82f518bc7843f4a316
河野浩生 <61747641koki@gmail.com>
2020-10-04 15:51:45 +0900

    add: firebase setting, dotenv, editorconfig

--- front/next.config.js
+++ front/next.config.js
@@ -1,12 +1,24 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
 
 module.exports = {
-	webpack: (config) => {
-		config.resolve.alias = {
-			...config.resolve.alias,
-			src: path.resolve(__dirname, './src'),
-		};
-		return config;
-	},
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      src: path.resolve(__dirname, './src'),
+    };
+    return config;
+  },
+  env: {
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+    FIREBASE_DATABASE_URL: process.env.FIREBASE_DATABASE_URL,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+    FIREBASE_MESSAGEING_SENDER_ID: process.env.FIREBASE_MESSAGEING_SENDER_ID,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
+  },
 };
--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,7 +1,25 @@
 import { AppProps } from 'next/app';
-
 import Head from 'next/head';
+import firebase from 'firebase';
+import 'firebase/analytics';
+
 import '../styles/globals.css';
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGEING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
+// Initialize Firebase
+if (typeof window !== 'undefined' && !firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+}
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
