03646514efdd90b8add07d913fc9924e8733d73c
河野浩生 <61747641koki@gmail.com>
2020-10-04 13:57:16 +0900

    fix bug

--- front/src/pages/_app.js
+++ front/src/pages/_app.js
@@ -1,12 +1,15 @@
 import Head from 'next/head'
 import '../styles/globals.css'
 
-export default function() {
+function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
       </Head>
+      <Component {...pageProps} />
     </>
-  )
+    )
 }
+
+export default MyApp
