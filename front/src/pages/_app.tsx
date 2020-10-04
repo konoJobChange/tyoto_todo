b9d753526d53e551effd72b619c7494decd6ff3b
河野浩生 <61747641koki@gmail.com>
2020-10-04 14:42:35 +0900

    setting: eslint prettier

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,17 +1,20 @@
-import { AppProps } from 'next/app'
+import { AppProps } from 'next/app';
 
-import Head from 'next/head'
-import '../styles/globals.css'
+import Head from 'next/head';
+import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return (
-    <>
-      <Head>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-    )
-}
+	return (
+		<>
+			<Head>
+				<link
+					rel="stylesheet"
+					href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+				/>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
+};
 
-export default MyApp
+export default MyApp;
