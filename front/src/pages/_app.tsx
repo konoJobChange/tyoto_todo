fd8d62264a4f64d008067dd6782dde125fe7bf4c
ohteru <arai_daiki@cam-inc.co.jp>
2020-10-04 14:06:20 +0900

    Update: _app.tsx

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,7 +1,9 @@
+import { AppProps } from 'next/app'
+
 import Head from 'next/head'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
