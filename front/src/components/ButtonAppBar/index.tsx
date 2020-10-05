1616b4ee3f934a26d127c8f365ba9607057458a7
河野浩生 <61747641koki@gmail.com>
2020-10-05 23:44:50 +0900

    click menu

--- front/src/components/ButtonAppBar/index.tsx
+++ front/src/components/ButtonAppBar/index.tsx
@@ -1,8 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import { Avatar, AppBar, Button, Toolbar, Typography } from '@material-ui/core';
-import { deepPurple } from '@material-ui/core/colors';
+import {
+  Avatar,
+  AppBar,
+  Button,
+  Toolbar,
+  Typography,
+  Popover,
+  List,
+  ListItemText,
+  ListItem,
+  Divider,
+  ListItemIcon,
+} from '@material-ui/core';
+import { deepPurple, red } from '@material-ui/core/colors';
 import { useAuth } from 'src/modules/hooks/useAuth';
+import { useRouter } from 'next/router';
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -26,14 +41,19 @@ const useStyles = makeStyles((theme: Theme) =>
       color: theme.palette.getContrastText(deepPurple[500]),
       backgroundColor: deepPurple[500],
     },
+    logout: {
+      color: red[500],
+    },
   }),
 );
 
 const ButtonAppBar = () => {
   const classes = useStyles();
   const { login: handleLogin, logout: handleLogout, user } = useAuth();
+  const router = useRouter();
 
   const [isLogin, setIsLogin] = useState(false);
+  const [popoverEl, setPopoverEl] = React.useState<HTMLButtonElement | null>(null);
 
   useEffect(() => {
     if (!user) {
@@ -42,6 +62,22 @@ const ButtonAppBar = () => {
     }
     setIsLogin(true);
   }, [user, setIsLogin]);
+
+  const goToProfile = () => router.push('/profile');
+
+  const handleClick = useCallback(
+    (event: React.MouseEvent<HTMLButtonElement>) => {
+      setPopoverEl(event.currentTarget);
+    },
+    [setPopoverEl],
+  );
+
+  const handleClose = () => {
+    setPopoverEl(null);
+  };
+
+  const isOpen = Boolean(popoverEl);
+  const popoverId = isOpen ? 'menu-popover' : undefined;
 
   return (
     <div className={classes.root}>
@@ -56,7 +92,7 @@ const ButtonAppBar = () => {
               Login
             </Button>
           ) : (
-            <Button onClick={handleLogout}>
+            <Button onClick={handleClick} aria-describedby={popoverId}>
               <Avatar
                 className={classes.purple}
                 alt={user?.displayName ?? undefined}
@@ -66,6 +102,36 @@ const ButtonAppBar = () => {
           )}
         </Toolbar>
       </AppBar>
+      <Popover
+        id={popoverId}
+        open={isOpen}
+        anchorEl={popoverEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+      >
+        <List>
+          <ListItem key="profile" onClick={goToProfile}>
+            <ListItemIcon>
+              <AccountBoxIcon />
+            </ListItemIcon>
+            <ListItemText>プロフィール</ListItemText>
+          </ListItem>
+          <Divider />
+          <ListItem key="logout" onClick={handleLogout}>
+            <ListItemIcon>
+              <ExitToAppIcon />
+            </ListItemIcon>
+            <ListItemText className={classes.logout}>ログアウト</ListItemText>
+          </ListItem>
+        </List>
+      </Popover>
     </div>
   );
 };
