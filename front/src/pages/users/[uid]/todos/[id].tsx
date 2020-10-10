cd32d5a8e099ac20b56648e72a8a376691d2b437
ohteru <arai_daiki@cam-inc.co.jp>
2020-10-10 22:58:09 +0900

    modify layout

--- front/src/pages/users/[uid]/todos/[id].tsx
+++ front/src/pages/users/[uid]/todos/[id].tsx
@@ -3,12 +3,10 @@ import {
   Container,
   Grid,
   IconButton,
-  makeStyles,
   Typography,
 } from '@material-ui/core';
 import Skeleton from '@material-ui/lab/Skeleton';
 
-import { green } from '@material-ui/core/colors';
 import { useRouter } from 'next/router';
 import React, { useCallback } from 'react';
 import dayjs from 'dayjs';
@@ -20,25 +18,8 @@ import {
 import { useAuth } from 'src/modules/hooks/useAuth';
 import { useDetail } from 'src/modules/hooks/useTodos';
 
-const useStyles = makeStyles({
-  cardHeader: {
-    backgroundColor: green[500],
-  },
-  progressRoot: {
-    display: 'flex',
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
-
 export default function Profile() {
   const { user } = useAuth();
-  const classes = useStyles();
   const router = useRouter();
   const qid = router.query.id as string;
   const todo = useDetail(qid, user);
@@ -57,17 +38,19 @@ export default function Profile() {
 
   return (
     <Container>
-      <Grid container justify="space-around" alignItems="center">
-        <IconButton onClick={handleDelete}>
-          <DeleteIcon fontSize="large" />
-        </IconButton>
-        <IconButton onClick={handleEdit}>
-          <CreateIcon fontSize="large" />
-        </IconButton>
-        <IconButton onClick={handleHome}>
-          <AppsOutlinedIcon fontSize="large" />
-        </IconButton>
-      </Grid>
+      <Box mt={1} mb={2}>
+        <Grid container justify="space-around" alignItems="center">
+          <IconButton onClick={handleDelete}>
+            <DeleteIcon fontSize="large" />
+          </IconButton>
+          <IconButton onClick={handleEdit}>
+            <CreateIcon fontSize="large" />
+          </IconButton>
+          <IconButton onClick={handleHome}>
+            <AppsOutlinedIcon fontSize="large" />
+          </IconButton>
+        </Grid>
+      </Box>
 
       <Box m={1}>
         <Typography gutterBottom variant="subtitle1">
