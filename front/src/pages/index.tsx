b3a73f7a2004287758db5b75722036ca4908c0b4
河野浩生 <61747641koki@gmail.com>
2020-10-10 12:06:46 +0900

    test github actions

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -6,7 +6,7 @@ const IndexPage = () => {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>Create Next Tyoto Todo App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
